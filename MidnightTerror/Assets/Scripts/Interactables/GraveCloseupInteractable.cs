3ea6cb129adb75c89aee7cf0d1891077bdf4e9a5
Nikita Glubokov <ITVenomancer@gmail.com>
2023-09-16 19:18:50 +0300

    Functional grave closeup interaction

--- MidnightTerror/Assets/Scripts/Interactables/GraveCloseupInteractable.cs
+++ MidnightTerror/Assets/Scripts/Interactables/GraveCloseupInteractable.cs
@@ -8,16 +8,27 @@ public class GraveCloseupInteractable : MonoBehaviour, IInteractable
     [SerializeField] private GameObject _graveDugUp;
     [SerializeField] private string _requiredInteractionItem;
 
+    [SerializeField] private BoxCollider2D _graveInitialCollider;
+    [SerializeField] private BoxCollider2D _graveDiggingCollider;
+    private void Awake()
+    {
+        _graveDiggingCollider.enabled = false;
+        _graveInitialCollider.enabled = true;
+    }
+
     public void TryExecuteInteraction()
     {
         print("grave inetarction");
         if (!_graveBasic.activeSelf)
         {
             _graveBasic.SetActive(true);
+            _graveInitialCollider.enabled = false;
+            _graveDiggingCollider.enabled = true;
         }
         else if (InventoryManager.instance.TryToFindAndUseItem(_requiredInteractionItem))
         {
             _graveDugUp.SetActive(true);
+            _graveDiggingCollider.enabled = false;
         }
     }
 }
