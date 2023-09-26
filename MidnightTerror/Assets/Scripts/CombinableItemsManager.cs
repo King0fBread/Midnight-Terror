b27c3113cad1622b82145033804491de0a146a6d
Nikita Glubokov <ITVenomancer@gmail.com>
2023-09-26 20:14:58 +0300

    Base logic on combinable items without UI

--- MidnightTerror/Assets/Scripts/CombinableItemsManager.cs
+++ MidnightTerror/Assets/Scripts/CombinableItemsManager.cs
@@ -17,10 +17,37 @@ public class CombinableItemsManager : MonoBehaviour
         private InventorySlot _firstSlotWithACombinable;
         private InventorySlot _secondSlotWithACombinable;
 
+        private bool _firstSlotOccupied = false;
+        private bool _secondSlotOccupied = false;
 
+        public bool CheckIfSpriteIsCombinable(Sprite sprite)
+        {
+            return sprite == _firstCombinableItem || sprite == _secondCombinableItem;
+        }
+        public void PopulateAvailableSlot(InventorySlot slot)
+        {
+            if (!_firstSlotOccupied)
+            {
+                _firstSlotWithACombinable = slot;
+                _firstSlotOccupied = true;
+            }
+            else if (!_secondSlotOccupied)
+            {
+                _secondSlotWithACombinable = slot;
+                _secondSlotOccupied = true;
+            }
+            //enable merging button UI
+        }
     }
     public void CheckIfSlotHasCombinable(InventorySlot slot)
     {
         //foreach Combinable loop, if slot sprite == required sprite, fill 1 combinable slot etc. if 2 filled == place new item in inventory and remove combinables
+        foreach(Combinable combinable in _combinables)
+        {
+            if (combinable.CheckIfSpriteIsCombinable(slot.GetItemBySprite()))
+            {
+                combinable.PopulateAvailableSlot(slot);
+            }
+        }
     }
 }
