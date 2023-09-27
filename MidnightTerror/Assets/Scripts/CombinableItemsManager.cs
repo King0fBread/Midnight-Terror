12d505655c8fe93ffca8810cab10670143ded49b
Nikita Glubokov <ITVenomancer@gmail.com>
2023-09-27 20:29:50 +0300

    Working on UI from combinables

--- MidnightTerror/Assets/Scripts/CombinableItemsManager.cs
+++ MidnightTerror/Assets/Scripts/CombinableItemsManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class CombinableItemsManager : MonoBehaviour
 {
     [SerializeField] private List<Combinable> _combinables = new List<Combinable>();
+    [SerializeField] private GameObject _combineButton;
+    [SerializeField] private Combinable _availableCombinable;
 
     [System.Serializable]
     public class Combinable
@@ -18,36 +21,40 @@ public class CombinableItemsManager : MonoBehaviour
         private InventorySlot _secondSlotWithACombinable;
 
         private bool _firstSlotOccupied = false;
-        private bool _secondSlotOccupied = false;
 
         public bool CheckIfSpriteIsCombinable(Sprite sprite)
         {
             return sprite == _firstCombinableItem || sprite == _secondCombinableItem;
         }
-        public void PopulateAvailableSlot(InventorySlot slot)
+        public Combinable PopulateAvailableSlot(InventorySlot slot)
         {
             if (!_firstSlotOccupied)
             {
                 _firstSlotWithACombinable = slot;
                 _firstSlotOccupied = true;
+                return null;
             }
-            else if (!_secondSlotOccupied)
+            else
             {
                 _secondSlotWithACombinable = slot;
-                _secondSlotOccupied = true;
+                //UI
+                return this;
             }
-            //enable merging button UI
+            
         }
     }
     public void CheckIfSlotHasCombinable(InventorySlot slot)
     {
-        //foreach Combinable loop, if slot sprite == required sprite, fill 1 combinable slot etc. if 2 filled == place new item in inventory and remove combinables
         foreach(Combinable combinable in _combinables)
         {
             if (combinable.CheckIfSpriteIsCombinable(slot.GetItemBySprite()))
             {
-                combinable.PopulateAvailableSlot(slot);
+                _availableCombinable = combinable.PopulateAvailableSlot(slot);
             }
         }
+    }
+    public void CombineItems()
+    {
+        //place new item in inventory and remove combinables
     }
 }
