f6606a523683902b4d890e7e37476ca4dcc4144b
Nikita Glubokov <ITVenomancer@gmail.com>
2023-10-02 20:49:09 +0300

    Working on the unlit area mechanics

---
+++ MidnightTerror/Assets/Scripts/Interactables/UnlitForestInteractable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Properties;
+using UnityEngine;
+
+public class UnlitForestInteractable : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _requiredItem;
+
+    [SerializeField] private GameObject _blackOverlay;
+    [SerializeField] private GameObject _boxCutterItem;
+
+    private Animator _overlayAnimator;
+    private void Awake()
+    {
+        _overlayAnimator = _blackOverlay.GetComponent<Animator>();
+    }
+    public void TryExecuteInteraction()
+    {
+        if (InventoryManager.instance.TryToFindAndUseItem(_requiredItem))
+        {
+            StartCoroutine(PlayCameraFlashSequenceCoroutine());
+        }
+    }
+    private IEnumerator PlayCameraFlashSequenceCoroutine()
+    {
+        _overlayAnimator.Play("UnlitAreaBlackOverlay");
+        yield return new WaitForSeconds(1.5f);
+        _boxCutterItem.SetActive(true);
+    }
+}
