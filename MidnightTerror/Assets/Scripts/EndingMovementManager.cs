625ddcc17d31d2569ce0d095d80e591ba1ba4ea8
Nikita Glubokov <ITVenomancer@gmail.com>
2023-10-18 17:25:34 +0300

    Functional final area transition and movement logic

--- MidnightTerror/Assets/Scripts/EndingMovementManager.cs
+++ MidnightTerror/Assets/Scripts/EndingMovementManager.cs
@@ -6,11 +6,17 @@ using UnityEngine;
 
 public class EndingMovementManager : MonoBehaviour
 {
-    [SerializeField] private float _imageSizeIncreasePerStep;
-    [SerializeField] private int _totalAmountOfSteps;
+    [SerializeField] private Transform _endingCameraTransform;
+
+    [SerializeField] private GameObject _areaObject;
 
     [SerializeField] private GameObject _baseMovementUI;
     [SerializeField] private GameObject _endingMovementUI;
+
+    [SerializeField] private float _imageSizeIncreasePerStep;
+    [SerializeField] private int _totalAmountOfSteps;
+
+    private Camera _camera;
 
     private int _currentStep = 0;
 
@@ -27,10 +33,34 @@ public class EndingMovementManager : MonoBehaviour
         {
             _instance = this;
         }
+
+        _camera = Camera.main;
     }
     public void MoveToEndingArea()
     {
+        _camera.transform.position = _endingCameraTransform.position;
 
+        _baseMovementUI.SetActive(false);
+        _endingMovementUI.SetActive(true);
+    }
+    public void MoveForward()
+    {
+        if(_currentStep < _totalAmountOfSteps)
+        {
+            if(_currentStep < _totalAmountOfSteps)
+            {
+                _currentStep++;
+
+                _areaObject.transform.localScale += new Vector3(_imageSizeIncreasePerStep, _imageSizeIncreasePerStep, 0);
+                if(_currentStep == _totalAmountOfSteps - 1)
+                {
+                    _endingMovementUI.SetActive(false);
+                    //enable a ghost with an item?
+                }
+
+
+            }
+        }
     }
 
 }
